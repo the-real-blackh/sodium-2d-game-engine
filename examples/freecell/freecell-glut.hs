2ee1474607542f5271203bb1746e3fe1ec2b50ee
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2013-11-19 16:19:17 +1300

    Disable annoying background changing.

--- examples/freecell/freecell-glut.hs
+++ examples/freecell/freecell-glut.hs
@@ -5,8 +5,8 @@ import FRP.Sodium.GameEngine2D.Platform (engine)
 
 main = do
     game <- freecell [
-            Background "background.jpg" 1.98 10,
-            Background "s1.png" 1.98 10
+            Background "background.jpg" 1.98 10 {-,
+            Background "s1.png" 1.98 10 -}
         ]
     engine (GLUTArgs "freecell" "resources") game
 
